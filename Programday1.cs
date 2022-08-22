483fdc14e3e14995564b316ad2590877b1b27765
Nareer <76417566+Nareer@users.noreply.github.com>
2022-08-22 16:36:12 +0700

    Add files via upload
    mfu day1 in com pro

---
+++ Programday1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6531501068_Lap01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("My name is Nareerak Chauyook");
+        }
+    }
+}
